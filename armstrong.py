d1e3b3e742b7b92ab22531e58c538df3223af78b
Shaik Sameer <shaiksameer160@gmail.com>
2023-04-19 11:49:27 +0530

    newprgrm

---
+++ armstrong.py
@@ -0,0 +1,12 @@
+num=int(input("Enter a number to be reversed:"))
+int sum=0
+int rem
+temp=num
+while(num!=0):
+    rem=num%10
+    sum=sum+rem*rem*rem
+    num=num/10
+if(sum==temp):
+    print("The number is armstrong")
+else:
+    print("the number is not an armstrong")
